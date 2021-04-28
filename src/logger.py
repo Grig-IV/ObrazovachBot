54c0905c60a10d29abcb99944f99430b29ce145e
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-04-28 22:47:37 +0300

    Make LoggerBot static, no singelton

--- src/logger.py
+++ src/logger.py
@@ -1,21 +1,15 @@
 from telebot import TeleBot
 
 class LoggerBot:
-    _bot_singelton = None
+    _tb = None
     _admin_id = None
-    
-    def create(bot_token, admin_id):
-        if not LoggerBot._bot_singelton:
-            LoggerBot._bot_singelton = TeleBot(bot_token)
-            _admin_id = admin_id
 
-        return LoggerBot._bot_singelton
+    @property
+    def is_enabled():
+        return _tb != None
 
-    def get_bot():
-        if not LoggerBot._bot_singelton:
-            return Exception("LoggerBot not created yet")
+    def enable(telebot):
+        LoggerBot._tb = telebot
 
-        return LoggerBot._bot_singelton
-
-    def send_log(self, package):
-        LoggerBot._bot_singelton.send_message(LoggerBot._admin_id, "pass")
+    def send_log(package):
+        pass
