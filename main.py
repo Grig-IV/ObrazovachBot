e1ec7acfc533d7cd13f80d1894de13d320ecd753
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-05 12:54:02 +0300

    implement parse_command and '/start' command

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import startup
-from src.logger import LoggerBot
-from src.message_manager import MessageManager
+from src.Services.logger import Logger
 
 
 obrz_bot = startup.build_obrz_bot()
 
 
-@obrz_bot.tb.middleware_handler(update_types=['message'])
+@obrz_bot.telebot.middleware_handler(update_types=['message'])
 def middleware_handler(bot_instance, package):
-    if LoggerBot.is_enabled:
-        LoggerBot.send_log(package)
+    if Logger.is_enabled:
+        Logger.send_log(package)
 
-    pikcher = obrz_bot.pikchers.get_or_create_pikcher(package)
+    pikcher = obrz_bot.pikcher_storage.get_or_create_user(package)
 
     is_bot_initialized = obrz_bot.initialization_handler(pikcher, package)
 
@@ -22,22 +21,24 @@ def middleware_handler(bot_instance, package):
         package.access_token = False
 
 
-@obrz_bot.tb.message_handler(content_types=['text'])
+@obrz_bot.telebot.message_handler(content_types=['text'])
 def commands_handler(message):
     if not message.access_token:
         return
 
-    parser = lambda t: (t.split()[0].strip(), t.split()[1:].strip())
-    command, value = parser(message.text)
+    command, value = obrz_bot.parse_command(message.text)
     pikcher = message.pikcher
 
-    error = None
-    if command in ['/беру', '/подумаю']:
-        error = obrz_bot.article_manager.take_article(pikcher, value)
+    if command == '/start':
+        obrz_bot.db_manager.create_db_message(pikcher)
+        obrz_bot.save_db()
+        obrz_bot.article_modul.create_article_message(pikcher)
+    elif command in ['/беру', '/подумаю']:
+        obrz_bot.article_modul.take_article(pikcher, value)
     elif command == '/на_опрос':
-        error = obrz_bot.article_manager.take_poll(pikcher, value)
+        obrz_bot.article_modul.take_poll(pikcher, value)
     elif command ==  '/не_беру':
-        error = obrz_bot.article_manager.give_article_back(pikcher, value)
+        obrz_bot.article_modul.give_article_back(pikcher, value)
     elif command.startswith('/add_filter '):
         pass
     elif command.startswith('/remove_filter '):
@@ -47,11 +48,8 @@ def commands_handler(message):
     else:
         pass
 
-    if error is not None:
-        MessageManager.send_message(message.chat_id, error)
 
-
-@obrz_bot.tb.callback_query_handler(func=lambda call: True)
+@obrz_bot.telebot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
     if not call.access_token:
         return
@@ -65,11 +63,11 @@ def callback_handler(call):
     elif action == 'refresh':
         pass
 
-@obrz_bot.tb.message_handler(content_types=['document'])
+@obrz_bot.telebot.message_handler(content_types=['document'])
 def doc_for_init_handler(db_message):
     print(db_message.access_token)
     
     pass
 
 
-obrz_bot.tb.polling(none_stop=True)
+obrz_bot.telebot.polling(none_stop=True)
