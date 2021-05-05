2e6507d7200670fc11957d8d54f323afe3af2e52
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-05 12:56:48 +0300

    implement abstract_message

--- src/AbstractClasses/abstract_message.py
+++ src/AbstractClasses/abstract_message.py
@@ -1,7 +1,19 @@
 class Message:
-    def __init__(self):
-        self.text = str()
-        self.parse_mode = 'HTML'
+    def __init__(self, text):
+        self.text = text
+        self.parse_mode = None
+        self.disable_web_page_preview = True
+        self.reply_markup = None
 
     def get_kwargs(self):
-        raise NotImplementedError("Subclass must implement get_kwargs method")
+        kwargs = dict()
+
+        kwargs['text'] = self.text
+        kwargs['disable_web_page_preview'] = disable_web_page_preview
+
+        if parse_mode is not None:
+            kwargs['parse_mode'] = self.parse_mode
+        if reply_markup is not None:
+            kwargs['reply_markup'] = self.reply_markup
+
+        return kwargs
