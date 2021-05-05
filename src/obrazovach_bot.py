a924626f465e0de98eeebb51f48c287deae75027
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-05 12:54:34 +0300

    clean up and implement parse_command

--- src/obrazovach_bot.py
+++ src/obrazovach_bot.py
@@ -3,36 +3,43 @@ from telebot import types
 from src.ArticleModul.articles_modul import ArticleModul
 from src.Services.message_service import MessageService
 from src.Messages.init_message import InitMessage
+from src.Services.telebot_provider import TelebotProvider
+from src.UserStorage.user_storage import UserStorage
+from src.DatabaseManager.database_manager import DatabaseManager
 
 
 class ObrazovachBot:
     def __init__(self, telebot, pikchers_names):
         self._is_initialized = False
 
-        self.moduls = list()
-        article_modul = ArticleModul(telebot)
-        self.moduls.append(article_modul)
+        self.telebot = telebot
 
-        self.pikchers_rep = PikchersRep(pikchers_names)
+        self.moduls = list()
+        self.article_modul = ArticleModul(self, telebot)
+        self.moduls.append(self.article_modul)
+
+        self.pikcher_storage = UserStorage(pikchers_names)
         self.db_manager = DatabaseManager(telebot)
 
     def initialization_handler(self, pikcher, package):
         if self._is_initialized:
             return True
-        
-        # TODO: сделать обработку пустой инициализации
 
-        if type(package) == types.Message:
-            MessageService.send_temporary_message(pikcher.chat_id,
-                                                  InitMessage())
-            return False
-        elif type(package) == types.CallbackQuery:
+        if type(package) == types.CallbackQuery:
             _, _, db_message_id = package.data.split()
             self.load_db(db_message_id)
-            return True
+            self._is_initialized = True
+        elif type(package) == types.Message:
+            if package.text == '/skip_init':
+                self._is_initialized = True
+            else:
+                telebot = TelebotProvider.get_telebot()
+                pass
 
-    def load_db(self, db_message_id):
-        db_dict = self.db_manager.load_db(db_message_id)
+        return self._is_initialized
+
+    def load_db(self, chat_id, db_message_id):
+        db_dict = self.db_manager.load_db(chat_id, db_message_id)
 
         self.pikchers_rep.set_pikchers_db(db_dict)
         for modul in self.moduls:
@@ -41,12 +48,21 @@ class ObrazovachBot:
     def save_db(self):
         db_dict = dict()
         
-        db_name, db = self.pikchers_rep.get_pikchers_db()
+        db_name, db = self.pikcher_storage.get_users_db()
         db_dict[db_name] = db
         for modul in self.moduls:
             db_name, db = modul.get_db()
             db_dict[db_name] = db
 
-        pikchers = self.pikchers_rep.pikchers
+        pikchers = self.pikcher_storage.get_users()
         self.db_manager.save_db(pikchers, db_dict)
+
+    def parse_command(self, command):
+        if ' ' not in command:
+            return command, None
+        
+        command, value = map(lambda s: s.strip(), command.split())
+        return command, value
+
+
 
