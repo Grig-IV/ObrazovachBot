7d0faf575ff65137329ca1d6928932e978f4f77b
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-06 13:39:29 +0300

    cleanup

--- src/ArticleModul/articles_modul.py
+++ src/ArticleModul/articles_modul.py
@@ -9,15 +9,10 @@ class ArticleModul:
         self.article_db = ArticleDataBase()
 
     def get_db(self):
-        return 'articleModul', 'Test'
-   
-    def create_article_message(self, pikcher):
-        telebot = TelebotProvider.get_telebot()
+        return 'articleModul', self.article_db.get_db()
 
-        a_mes = ArticleMessage()
-
-        telebot.send_message(pikcher.chat_id, **a_mes.get_kwargs())
-
+    def set_db(self, dict_db):
+        self.article_db.set_db(dict_db['articleModul'])
 
     # Decorator
     def update_article_db(func):
@@ -36,37 +31,51 @@ class ArticleModul:
             pikchers_f = lambda p: p.article_message_id is not None
             pikchers = self.obrz_bot.pikchers_rep.get_pikchers(pikchers_f)
 
-            free_articles_f = lambda a: a.taken_by is None
-            free_a = self.article_db.get_articles(free_articles_f)
+            free_articles_filter = self.article_db.filters['free_articles']
+            free_articles = self.article_db.get_articles(free_articles_filter)
 
             telebot = TelebotProvider.get_telebot()
-            for p in pikchers:
-                a_mes = ArticleMessage(p, free_a, None)
-                telebot.edit_message(p.chat_id,
-                                     p.article_message_id,
+            for pikcher in pikchers:
+                a_mes = ArticleMessage(pikcher,
+                                       free_articles,
+                                       self.article_db.last_update)
+                telebot.edit_message(pikcher.chat_id,
+                                     pikcher.article_message_id,
                                      **a_mes.get_kwargs())
 
         return func_with_updater
 
     @update_messages
     @update_article_db
+    def create_article_message(self, pikcher):            
+        free_articles_filter = self.article_db.filters['free_articles']
+        free_articles = self.article_db.get_articles(free_articles_filter)
+        a_mes = ArticleMessage(pikcher,
+                               free_articles,
+                               self.article_db.last_update)
+
+        telebot = TelebotProvider.get_telebot()
+        telebot.send_message(pikcher.chat_id, **a_mes.get_kwargs())
+
+    @update_messages
+    @update_article_db
     def take_article(self, pikcher, article_url):
-        article = self.article_db.get_article(article_url)
+        article = self.article_db.find_article(article_url)
 
         article.taken_by = pikcher.username
 
     @update_messages
     @update_article_db
     def take_poll(self, pikcher, article):
-        article = self.article_db.get_article(article_url)
+        article = self.article_db.find_article(article_url)
 
         article.use_for_poll()
 
     @update_messages
     def give_article_back(self, pikcher, article):
-        article = self.article_db.get_article(article_url)
+        article = self.article_db.find_article(article_url)
 
         if article.taken_by is not None:
             article.taken_by = None
-        elif article.for_poll:
+        elif article.is_for_poll:
             article.dont_use_for_poll()
