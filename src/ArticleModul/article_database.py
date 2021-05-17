a4253025ba4d4b8ee198239c405e424886bde6c2
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-17 10:25:40 +0300

    split _articles to _free_articles _taken_articles

--- src/ArticleModul/article_database.py
+++ src/ArticleModul/article_database.py
@@ -1,3 +1,6 @@
+import asyncio
+import datetime as dt
+
 from src.ArticleModul.nplus1_parser import Parser_NP1
 from src.ArticleModul.Models.article import Article
 
@@ -5,41 +8,56 @@ from src.ArticleModul.Models.article import Article
 class ArticleDataBase:
     def __init__(self):
         self._last_update = None
-        self._articles = list()
-
-        self.filters = {'free_articles': lambda a: a.taken_by is None,
-                        'taken_articles': lambda a: a.taken_by is not None}
+        self._free_articles = list()
+        self._taken_articles = list()
 
     @property
     def last_update(self):
         return self._last_update
 
+    @property
+    def free_articles(self):
+        return self._free_articles.copy()
+
+    @property
+    def taken_articles(self):
+        return self._taken_articles.copy()
+
+    @property
+    def all_articles(self):
+        return self.free_articles + self.taken_articles
+
     def get_db(self):
-        return list(map(lambda a: a.to_dict(), self._articles))
+        free_articles = list(map(lambda a: a.to_dict(), self.free_articles))
+        taken_articles = list(map(lambda a: a.to_dict(), self.taken_articles))
+        db = {'freeArticles': free_articles, 'takenArticles': taken_articles}
+        return db
 
-    def set_db(self, article_dict_list):
-        self._articles = list(map(lambda d: Article(**d), article_dict_list))
-
-    def get_articles(self, key_func=None):
-        if key_func is None:
-            return self._articles.copy()
-        
-        articles = list(filter(key_func, self._articles))
-        return articles
+    def set_db(self, db):
+        free_articles = map(lambda d: Article(**d), db['freeArticles'])
+        taken_articles = map(lambda d: Article(**d), db['takenArticles'])
+        self._free_articles = list(free_articles)
+        self._taken_articles = list(taken_articles)
 
     def find_article(self, article_url):
-        for article in self._articles:
-            if article.url == article_url:
-                return article
+        finding_g = (a for a in self.all_articles if a.url == article_url)
+        return next(finding_g, None)
 
-        return None
+    async def update(self):
+        MSK_TZ = dt.timezone(dt.timedelta(hours=3))
+        self._last_update = dt.datetime.now(MSK_TZ)
 
-    def update(self):
         links_on_main = Parser_NP1.get_links_on_main()
-        db_links = map(lambda a: a.link, self._articles)
+        db_links = map(lambda a: a.link, self.all_articles)
         new_links = list(filter(lambda l: l not in db_links, links_on_main))
-        
+
+        tasks = list()
         for link in new_links:
-            article_dict = Parser_NP1.parse_article(link)
-            article = Article(**article_dict)
-            self._articles.append(article)
+            task = asyncio.create_task(Parser_NP1.parse_article(link))
+            tasks.append(task)
+
+        article_dicts = await asyncio.gather(*tasks)
+
+        for a_dict in article_dicts:
+            article = Article(**a_dict)
+            self._free_articles.append(article)
