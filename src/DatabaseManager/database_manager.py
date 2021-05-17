f4b34c3670215d6f76881be0c085a323f67b57b4
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-17 10:35:49 +0300

    cleanup

--- src/DatabaseManager/database_manager.py
+++ src/DatabaseManager/database_manager.py
@@ -1,3 +1,4 @@
+import requests
 import json
 
 from telebot.types import InputMediaDocument
@@ -13,13 +14,13 @@ class DatabaseManager:
         with open('PlaceholderFile', 'rb') as db_file:
             db_message = self._tb.send_document(user.chat_id, db_file)
             db_message_id = db_message.message_id
-            user.set_data('db_message_id', db_message_id)
+            user.set_data('databaseMessageId', db_message_id)
 
-    def load_db(chat_id, db_message_id):
+    def load_db(self, chat_id, db_message_id):
         db_message = self._get_db_message(chat_id, db_message_id)
         file_path = self._tb.get_file(db_message.document.file_id).file_path
         FILE_URL = 'https://api.telegram.org/file/bot{0}/{1}'
-        db_str = requests.get(FILE_URL.format(tb.token, file_path)).text
+        db_str = requests.get(FILE_URL.format(self._tb.token, file_path)).text
 
         db_dict = json.loads(db_str)
 
@@ -30,8 +31,8 @@ class DatabaseManager:
             json.dump(db_dict, db_file)
         with open('bot_database.json', 'rb') as db_file:
             for user in users:
-                db_message_id = user.data.get('db_message_id')
-                if  db_message_id is None:
+                db_message_id = user.data.get('databaseMessageId')
+                if db_message_id is None:
                     continue
 
                 self._tb.edit_message_media(
