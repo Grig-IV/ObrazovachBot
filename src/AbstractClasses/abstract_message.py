fcc087b2884561c40599ea5b7799767172b2d556
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-05-17 10:24:36 +0300

    rework AbstractMessage

--- src/AbstractClasses/abstract_message.py
+++ src/AbstractClasses/abstract_message.py
@@ -1,19 +1,21 @@
 class Message:
-    def __init__(self, text):
+    def __init__(self, text, parse_mode=None,
+                 disable_web_page_preview=True,
+                 reply_markup=None):
         self.text = text
-        self.parse_mode = None
-        self.disable_web_page_preview = True
-        self.reply_markup = None
+        self.parse_mode = parse_mode
+        self.disable_web_page_preview = disable_web_page_preview
+        self.reply_markup = reply_markup
 
     def get_kwargs(self):
         kwargs = dict()
 
         kwargs['text'] = self.text
-        kwargs['disable_web_page_preview'] = disable_web_page_preview
+        kwargs['disable_web_page_preview'] = self.disable_web_page_preview
 
-        if parse_mode is not None:
+        if self.parse_mode is not None:
             kwargs['parse_mode'] = self.parse_mode
-        if reply_markup is not None:
+        if self.reply_markup is not None:
             kwargs['reply_markup'] = self.reply_markup
 
         return kwargs
